463e4a47cf204f9782bdc32b36c501162f04d4fe
Iloyuk <jasonsima2006@gmail.com>
2024-05-16 17:40:34 -0400

    did some stuff

--- src/Display.java
+++ src/Display.java
@@ -3,10 +3,10 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Display extends JPanel {
+
     private final int dispWidth, dispHeight;
     private ArrayList<Ball> ballList;
-
-    private int count = 0;
+    private int choice; // 0 = quit, 1 = launch ball, 2 = ball dropping
 
     public Display() {
         super();
@@ -19,8 +19,25 @@ public class Display extends JPanel {
         return new Dimension(dispWidth, dispHeight);
     }
 
+    public int showMenu() {
+        String[] options = {"Quit", "Launching Ball", "Ball Dropping"};
+        int choice = JOptionPane.showOptionDialog(this,
+                "What would you like to simulate now?", "Physics Simulation",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
+
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                System.exit(0);
+        }
+
+        return choice;
+    }
+
     public void update() {
-        System.out.println(count++);
         repaint();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,35 @@ public class Main {
 
     public static final int WIDTH = 1800;
     public static final int HEIGHT = 1000;
+    static int choice;
 
     public static void main(String[] args) {
 
-        Display display = new Display();
-        display.setLayout(null);
-
-        Timer timer = new Timer(10, ActionEvent -> display.update());
-
-        JButton btnStop = getBtnStop(timer);
-
-        display.add(btnStop);
-
         JFrame frame = new JFrame("Physics");
+        Display display = new Display();
+        Timer timer = new Timer(10, ActionEvent -> {
+            display.update();
+            if (choice == 0)
+                frame.dispose();
+        });
+
+        display.setLayout(null);
+        display.add(getBtnStop(timer, display));
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         frame.setBounds(0, 0, WIDTH, HEIGHT);
-        frame.setContentPane(display);
-        frame.setVisible(true);
+        choice = display.showMenu();
 
-        timer.start();
-
+        if (choice != 0) {
+            timer.start();
+            frame.setContentPane(display);
+            frame.setVisible(true);
+        }
+        else System.exit(0);
     }
 
-    private static JButton getBtnStop(Timer timer) {
+    private static JButton getBtnStop(Timer timer, Display display) {
 
         JButton btnStop = new JButton();
         btnStop.setFont(new Font("SansSerif", Font.BOLD, 30));
@@ -39,11 +44,10 @@ public class Main {
         btnStop.addActionListener(e -> {
             if (e.getSource() == btnStop) {
                 timer.stop();
-                JOptionPane.showInputDialog(null, "What height");
+                choice = display.showMenu();
                 timer.start();
             }
         });
-        btnStop.setLayout(null);
 
         return btnStop;
     }
